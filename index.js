a3275c8a81859c0275e962f4822162cb55337c4f
Ismail Hasan <ismailhasan0132@gmail.com>
2020-05-26 20:35:56 -0700

    Added skeletons for commands

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const Discord = require('discord.js');
-const config = require('./config.json')
+const config = require('./config.json');
+const data = require('./data.json');
 const client = new Discord.Client();
 const webhookClient = new Discord.WebhookClient(config.webhook_id, config.webhook_token);
 
 client.once('ready', () => {
     console.log('ready');
+    //check if server has data
+    //create webhook
+    //store webhook in json
+    //nice
 });
 
 
@@ -13,7 +18,16 @@ client.login(config.bot_token);
 
 
 client.on("message", async message => {
-        
+    console.log("working");
+    if(message.content.startsWith('$add')){
+        let user = message.mentions.members.first().id;
+        // add user id to json
+    }
+
+    if(message.content.startsWith == "$remove"){
+        let user = message.mentions.members.first().id;
+        // remove user id from json, shift all elements over.
+    }
 });
 
 
