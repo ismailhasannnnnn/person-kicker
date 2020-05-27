8bae0511b9da9ae3c2e685556da6ecaaa9d0102b
MichaelMatar <michaelfm1415@gmail.com>
2020-05-26 19:51:36 -0700

    deleted all commands

--- index.js
+++ index.js
@@ -13,29 +13,19 @@ client.login(config.bot_token);
 
 
 client.on("message", async message => {
-        if(message.content == "later chris" && message.author.bot) {
+        
+});
+
+
+
+
+
+/*
+if(message.content == "later chris" && message.author.bot) {
 
             let user =  await client.users.fetch("292885865074655242");
             let chris = message.guild.member(user);
             chris.voice.kick();
 
-        } else if(message.content == "cya ismail" && message.author.bot) {
-
-            let user =  await client.users.fetch("132331615056691200");
-            let ismail = message.guild.member(user);
-            ismail.voice.kick();
-
-        } else if(message.content == "cya michael" && message.author.bot) {
-
-            let user =  await client.users.fetch("191754197203550208");
-            let michael = message.guild.member(user);
-            michael.voice.kick();
-
-        } else if(message.content == "cya marcus" && message.author.bot) {
-
-            let user =  await client.users.fetch("216326109862690816");
-            let marcus = message.guild.member(user);
-            marcus.voice.kick();
-
         }
-});
+*/
