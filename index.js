27f98a6d8cd8aea2f05f4a04b405f3665389d314
Ismail Hasan <ismailhasan0132@gmail.com>
2020-05-28 19:55:37 -0700

    Added $setchannel command to set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const data = require('./data.json');
+const fs = require('fs'); //allows reading and writing to json file
 const client = new Discord.Client();
 const webhookClient = new Discord.WebhookClient(config.webhook_id, config.webhook_token);
 
@@ -12,60 +13,13 @@ client.once('ready', () => {
     //nice
 });
 
-console.log(data.Guild.ServerData.ServerID);
+// console.log(data.Guild.ServerData.ServerID);
 
 client.on("guildCreate", function (guild) {
-    const fs = require('fs') //allows reading and writing to json file
 
     //check if server has data
     //let serverID = guild.id;
-    fs.readFile('./data.json', (err, jsonString) => {
-        if (err) {
-            console.log("File read failed:", err)
-            return
-        }
 
-        try {
-
-            let guildName = guild.name.replace(/\s+/g, ''); //removes whitespace from string
-            const dataJson = JSON.parse(jsonString);
-
-            if (data.Guilds.length == 0) {
-                
-                let guildName = guild.name.replace(/\s+/g, ''); //removes whitespace from string
-                let newJson = { [guildName]: { "ServerData": {"serverId" : [guild.id]}, "UserData": [] } };
-                dataJson.Guilds.push(newJson);
-                let jsonStr = JSON.stringify(dataJson);
-
-                fs.writeFile("./data.json", jsonStr, function (err) {
-                    if (err) throw err;
-                    console.log('Saved!');
-                });
-            }
-
-            // for (let i = 0; i < data.Guilds.length; i++) {
-            //     console.log("running setup")
-            //     if (data.Guilds[i].) {
-            //         break;
-            //     } else if (i == data.Guilds.length - 1 && data.Guilds[i].ServerData.serverId != guild.id) {
-            //         const dataJson = JSON.parse(jsonString)
-            //         let newJson = { [guildName]: { "ServerData": {"serverId" : [guild.id]}, "UserData": [] } };
-            //         dataJson.Guilds.push(newJson);
-            //         let jsonStr = JSON.stringify(dataJson);
-
-            //         fs.writeFile("./data.json", jsonStr, function (err) {
-            //             if (err) throw err;
-            //             console.log('Saved!');
-            //         });
-            //     }
-            // }
-            
-            
-            
-        } catch (err) {
-            console.log('Error parsing JSON string:', err)
-        }
-    })
 
 
 
@@ -82,14 +36,37 @@ client.login(config.bot_token);
 
 client.on("message", async message => {
     console.log("working");
-    if(message.content.startsWith('$add')){
+    if (message.content.startsWith('$add')) {
         let user = message.mentions.members.first().id;
         // add user id to json
     }
 
-    if(message.content.startsWith == "$remove"){
+    if (message.content.startsWith == "$remove") {
         let user = message.mentions.members.first().id;
         // remove user id from json, shift all elements over.
+    }
+
+    if (message.content == "$setchannel") {
+
+        let channelId = message.channel.id;
+        let guildname = message.guild.name.replace(/\s+/g, '');
+        let serverData = data.Guilds[0][guildname].ServerData;
+
+        if ("webhookChannelId" in serverData) {
+            if (serverData["webhookChannelId"] !== channelId) {
+                serverData["webhookChannelId"] = channelId;
+                message.channel.send("Webhook channel updated successfully!");
+            } else {
+                message.channel.send("This is the same webhook channel!");
+            }
+        } else {
+            serverData.webhookChannelId = channelId;
+            message.channel.send("Webhook set to send messages to this channel!");
+        }
+
+        fs.writeFile("./data.json", JSON.stringify(data, null, 4), function (err) {
+            if (err) throw err;
+        });
     }
 });
 
