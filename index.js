540d633e82089d528a92575926e8fdd5672a8269
Ismail Hasan <ismailhasan0132@gmail.com>
2020-05-28 18:17:52 -0700

    Added data.json manipulation code

--- index.js
+++ index.js
@@ -14,24 +14,60 @@ client.once('ready', () => {
 
 console.log(data.Guild.ServerData.ServerID);
 
-client.on("guildCreate",function(guild){
+client.on("guildCreate", function (guild) {
     const fs = require('fs') //allows reading and writing to json file
 
     //check if server has data
-    //let serverID = client.guilds.id;
-    
+    //let serverID = guild.id;
     fs.readFile('./data.json', (err, jsonString) => {
         if (err) {
             console.log("File read failed:", err)
             return
         }
+
         try {
-            const guildInfo = JSON.parse(jsonString)
-    } catch(err) {
+
+            let guildName = guild.name.replace(/\s+/g, ''); //removes whitespace from string
+            const dataJson = JSON.parse(jsonString);
+
+            if (data.Guilds.length == 0) {
+                
+                let guildName = guild.name.replace(/\s+/g, ''); //removes whitespace from string
+                let newJson = { [guildName]: { "ServerData": {"serverId" : [guild.id]}, "UserData": [] } };
+                dataJson.Guilds.push(newJson);
+                let jsonStr = JSON.stringify(dataJson);
+
+                fs.writeFile("./data.json", jsonStr, function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                });
+            }
+
+            // for (let i = 0; i < data.Guilds.length; i++) {
+            //     console.log("running setup")
+            //     if (data.Guilds[i].) {
+            //         break;
+            //     } else if (i == data.Guilds.length - 1 && data.Guilds[i].ServerData.serverId != guild.id) {
+            //         const dataJson = JSON.parse(jsonString)
+            //         let newJson = { [guildName]: { "ServerData": {"serverId" : [guild.id]}, "UserData": [] } };
+            //         dataJson.Guilds.push(newJson);
+            //         let jsonStr = JSON.stringify(dataJson);
+
+            //         fs.writeFile("./data.json", jsonStr, function (err) {
+            //             if (err) throw err;
+            //             console.log('Saved!');
+            //         });
+            //     }
+            // }
+            
+            
+            
+        } catch (err) {
             console.log('Error parsing JSON string:', err)
-        } 
+        }
     })
-        console.log(data.Guild.ServerData.ServerID);
+
+
 
     //create webhook
     //send webhook to server general chat
