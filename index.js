fa950f745e613c88672313781d2f3b0efb52247e
Ismail Hasan <ismailhasan0132@gmail.com>
2020-05-28 18:04:07 -0700

    added some code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const data = require('./data.json');
 const client = new Discord.Client();
-const webhookClient = new Discord.WebhookClient(config.webhook_id, config.webhook_token);
 
 client.once('ready', () => {
     console.log('ready');
@@ -12,23 +11,60 @@ client.once('ready', () => {
     //nice
 });
 
-client.guildCreate("guildCreate",function(guild){
+client.on("guildCreate", function (guild) {
     const fs = require('fs') //allows reading and writing to json file
 
     //check if server has data
-    //let serverID = client.guilds.id;
-    
+    //let serverID = guild.id;
     fs.readFile('./data.json', (err, jsonString) => {
         if (err) {
             console.log("File read failed:", err)
             return
         }
+
         try {
-            const guildInfo = JSON.parse(jsonString)
-    } catch(err) {
+
+            let guildName = guild.name.replace(/\s+/g, ''); //removes whitespace from string
+            const dataJson = JSON.parse(jsonString);
+
+            if (data.Guilds.length == 0) {
+                
+                let guildName = guild.name.replace(/\s+/g, ''); //removes whitespace from string
+                let newJson = { [guildName]: { "ServerData": {"serverId" : [guild.id]}, "UserData": [] } };
+                dataJson.Guilds.push(newJson);
+                let jsonStr = JSON.stringify(dataJson);
+
+                fs.writeFile("./data.json", jsonStr, function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                });
+            }
+
+            // for (let i = 0; i < data.Guilds.length; i++) {
+            //     console.log("running setup")
+            //     if (data.Guilds[i].) {
+            //         break;
+            //     } else if (i == data.Guilds.length - 1 && data.Guilds[i].ServerData.serverId != guild.id) {
+            //         const dataJson = JSON.parse(jsonString)
+            //         let newJson = { [guildName]: { "ServerData": {"serverId" : [guild.id]}, "UserData": [] } };
+            //         dataJson.Guilds.push(newJson);
+            //         let jsonStr = JSON.stringify(dataJson);
+
+            //         fs.writeFile("./data.json", jsonStr, function (err) {
+            //             if (err) throw err;
+            //             console.log('Saved!');
+            //         });
+            //     }
+            // }
+            
+            
+            
+        } catch (err) {
             console.log('Error parsing JSON string:', err)
-        } 
+        }
     })
+
+
 
     //create webhook
     //send webhook to server general chat
@@ -42,13 +78,13 @@ client.login(config.bot_token);
 
 
 client.on("message", async message => {
-    console.log("working");
-    if(message.content.startsWith('$add')){
+
+    if (message.content.startsWith('$add')) {
         let user = message.mentions.members.first().id;
         // add user id to json
     }
 
-    if(message.content.startsWith == "$remove"){
+    if (message.content.startsWith == "$remove") {
         let user = message.mentions.members.first().id;
         // remove user id from json, shift all elements over.
     }
